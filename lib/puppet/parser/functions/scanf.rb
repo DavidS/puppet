0bfe7de4e26987dd6be2b34e3cd6ffffc0c683a8
David Schmitt <david@dasz.at>
2015-02-10 17:23:13 +0100

    (PUP-3991) Implement scanf function for classic parser

--- lib/puppet/parser/functions/scanf.rb
+++ lib/puppet/parser/functions/scanf.rb
@@ -1,3 +1,5 @@
+require 'scanf'
+
 Puppet::Parser::Functions::newfunction(
   :scanf,
   :type => :rvalue,
@@ -8,27 +10,17 @@ See the documenation of Ruby's String::scanf method for details about the suppor
 are similar but not identical to the formats used in Puppet's `sprintf` function.
 
 This function takes two mandatory arguments: the first is the String to convert, and the second
-the format String. A parameterized block may optionally be given, which is called with the result
-that is produced by scanf if no block is present, the result of the block is then returned by
-the function.
+the format String.
 
 The result of the scan is an Array, with each sucessfully scanned and transformed value.args The scanning
 stops if a scan is unsuccesful and the scanned result up to that point is returned. If there was no
-succesful scan at all, the result is an empty Array. The optional code block is typically used to
-assert that the scan was succesful, and either produce the same input, or perform unwrapping of
-the result
+succesful scan at all, the result is an empty Array.
 
+      scanf("42", "%i")[0] == 42
 
-      "42".scanf("%i")
-      "42".scanf("%i") |$x| {
-        unless $x[0] =~ Integer {
-          fail "Expected a well formed integer value, got '$x[0]'"
-        }
-        $x[0]
-      }
-
-- Since 4.0.0
 DOC
 ) do |args|
-  function_fail(["scanf() is only available when parser/evaluator future is in effect"])
+  data = args[0]
+  format = args[1]
+  result = data.scanf(format)
 end
